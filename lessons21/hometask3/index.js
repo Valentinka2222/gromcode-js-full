e9c7ef66ab99b912309a19f003c7167f575cb072
Valentinka2222 <xamalyan89@gmail.com>
2022-03-20 21:47:28 +0200

    3

--- lessons21/hometask3/index.js
+++ lessons21/hometask3/index.js
@@ -2,28 +2,27 @@
 
 export function getTitle() {
   let elementTitle = document.querySelector('.title').textContent;
-  console.dir(elementTitle);
+
   return elementTitle;
 }
 getTitle();
 
 export function getDescription() {
   let element = document.querySelector('.about').innerText;
-  console.dir(element);
+
   return element;
 }
 getDescription();
 
 export function getPlans() {
   let elementPlans = document.querySelector('.plans').innerHTML;
-  console.dir(elementPlans);
   return elementPlans;
 }
 getPlans();
 
 export function getGoal() {
   let elementGoal = document.querySelector('.goal').outerHTML;
-  console.dir(elementGoal);
+
   return elementGoal;
 }
 getGoal();
