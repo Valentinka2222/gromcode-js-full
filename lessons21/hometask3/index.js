e098fba91869189d1780b6f97ee0508109a60c39
Valentinka2222 <xamalyan89@gmail.com>
2022-03-21 11:28:16 +0200

    9

--- lessons21/hometask3/index.js
+++ lessons21/hometask3/index.js
@@ -1,21 +1,19 @@
-'use-strick';
+'use strict';
 
 function getTitle() {
   return document.querySelector('.title').textContent;
 }
-getTitle();
+
 function getDescription() {
   return document.querySelector('.about').innerText;
 }
-getDescription();
 
 function getPlans() {
   return document.querySelector('.plans').innerHTML;
 }
-getPlans();
 
 function getGoal() {
   return document.querySelector('.goal').outerHTML;
 }
-getGoal();
+
 export { getDescription, getGoal, getPlans, getTitle };
