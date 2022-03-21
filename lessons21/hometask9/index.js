624281a2b206c414b4a88db293be0f7fd3e80060
Valentinka2222 <xamalyan89@gmail.com>
2022-03-21 16:56:43 +0200

    L21ht8

---
+++ lessons21/hometask9/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export function finishForm() {
+  const elementForm = document.querySelector('.login-form');
+  const elementInputPassword = document.querySelector('input');
+  elementInputPassword.setAttribute('type', 'password');
+  const elementInput = document.createElement('input');
+  elementInput.classList.add('input-login');
+  elementInput.setAttribute('type', 'text');
+  elementInput.setAttribute('name', 'login');
+  elementForm.prepend(elementInput);
+}
