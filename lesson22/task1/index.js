bcc8f62b6bff1df14bd6afce3abe0749464a37bc
Valentinka2222 <xamalyan89@gmail.com>
2022-03-23 21:50:24 +0200

    L22t1

---
+++ lesson22/task1/index.js
@@ -0,0 +1,33 @@
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+
+const eventsListElem = document.querySelector('.events-list');
+function removeList() {
+  while (eventsListElem.firstChild) {
+    eventsListElem.removeChild(eventsListElem.firstChild);
+  }
+}
+removeList();
+
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color:${color}; margin-left: 8px;">${text}</span>`;
+};
+
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+
+divElem.addEventListener('click', logGreyDiv, true);
+divElem.addEventListener('click', logGreenDiv);
+pElem.addEventListener('click', logGreyP, true);
+pElem.addEventListener('click', logGreenP);
+spanElem.addEventListener('click', logGreySpan, true);
+spanElem.addEventListener('click', logGreenSpan);
+const clearBtn = document.querySelector('.clear-btn');
+
+clearBtn.removeEventListener('click', logTarget);
