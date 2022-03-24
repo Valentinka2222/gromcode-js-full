fa553e54dd406cef35db57798ac2980085775f5d
Valentinka2222 <xamalyan89@gmail.com>
2022-03-24 14:17:08 +0200

    1

--- lesson22/homatask3/index.js
+++ lesson22/homatask3/index.js
@@ -1,9 +1,15 @@
 const checkboxElem = document.querySelector('.task-status');
 const statusCheckbox = () => {
   if (checkboxElem.checked === true) {
+    console.log(checkboxElem.value);
     console.log('true');
   } else {
+    console.log(checkboxElem.value);
     console.log('false');
   }
 };
+const CheckboxText = () => {
+  alert(checkboxElem.checked);
+};
 checkboxElem.addEventListener('click', statusCheckbox);
+checkboxElem.addEventListener('change', CheckboxText);
