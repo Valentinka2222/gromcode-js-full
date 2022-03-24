d50d0560388060d12079323c0d0a916575b6d165
Valentinka2222 <xamalyan89@gmail.com>
2022-03-24 13:57:22 +0200

    1

--- lesson22/homatask3/index.js
+++ lesson22/homatask3/index.js
@@ -1,7 +1,8 @@
 const checkboxElem = document.querySelector('.task-status');
 function statusCheckbox() {
-  if (checkboxElem.checked === true) console.log('true');
-  else {
+  if (checkboxElem.checked === true) {
+    console.log('true');
+  } else {
     console.log('false');
   }
 }
