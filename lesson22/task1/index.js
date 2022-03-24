973f841a9ed1c5e9bab4c25616feab9cc0b8b12d
Valentinka2222 <xamalyan89@gmail.com>
2022-03-24 22:15:39 +0200

    1

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -46,11 +46,11 @@ const deleteEvent = () => {
   pElem.removeEventListener('click', logGreenP);
   spanElem.removeEventListener('click', logGreySpan, true);
   spanElem.removeEventListener('click', logGreenSpan);
-  divElem.removeEventListener('click', logGreyDiv);
+  divElem.removeEventListener('click', logGreyDiv, true);
   divElem.removeEventListener('click', logGreenDiv);
-  pElem.removeEventListener('click', logGreyP);
+  pElem.removeEventListener('click', logGreyP, true);
   pElem.removeEventListener('click', logGreenP);
-  spanElem.removeEventListener('click', logGreySpan);
+  spanElem.removeEventListener('click', logGreySpan, true);
   spanElem.removeEventListener('click', logGreenSpan);
 };
 
