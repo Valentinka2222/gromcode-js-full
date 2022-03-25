8403ff85927fe5c8b1bfafd360a6dc6017c96006
Valentinka2222 <xamalyan89@gmail.com>
2022-03-25 18:04:00 +0200

    L22ht9

---
+++ lesson22/homatask9/index.js
@@ -0,0 +1,9 @@
+const bodyBtn = document.querySelectorAll('.pagination__page');
+
+function handleClick(event) {
+  console.log(event.target.getAttribute('data-page-number'));
+}
+
+bodyBtn.forEach((button) => {
+  button.addEventListener('click', handleClick);
+});
