2ec951857e0903df2a4cf8e036b99bab8db4f559
Valentinka2222 <xamalyan89@gmail.com>
2022-03-25 16:16:41 +0200

    4

--- lesson22/homatask6/index.js
+++ lesson22/homatask6/index.js
@@ -1,7 +1,5 @@
 const bodyBtn = document.querySelector('body');
 function handleClick(event) {
-  if (event.target.nodeName === 'BUTTON') {
-    console.log(event.target.textContent);
-  }
+  console.log(event.target.textContent);
 }
 bodyBtn.addEventListener('click', handleClick);
