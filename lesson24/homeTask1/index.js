e0daa36e73a77ac4427e81100ae6d3f24292366b
Valentinka2222 <xamalyan89@gmail.com>
2022-03-31 21:04:17 +0300

    1

--- lesson24/homeTask1/index.js
+++ lesson24/homeTask1/index.js
@@ -1,19 +1,13 @@
 export const getDiff = (startDate, endDate) => {
-  if (startDate < endDate) {
-    let diff = endDate - startDate;
+  let diff = endDate - startDate;
 
-    diff /= 1000;
-    const days = Math.round(diff / 60 / 60 / 24);
-    const hours = Math.round((diff / 60 / 60) % 24);
-    const minutes = Math.round((diff / 60) % 60);
-    const seconds = Math.round(diff % 60);
+  diff /= 1000;
+  const days = Math.round(diff / 60 / 60 / 24);
+  const hours = Math.round((diff / 60 / 60) % 24);
+  const minutes = Math.round((diff / 60) % 60);
+  const seconds = Math.round(diff % 60);
 
-    const dateStr = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-    console.log(dateStr);
-    return dateStr;
-  }
+  const dateStr = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  console.log(dateStr);
+  return dateStr;
 };
-
-let dt1 = new Date('October 13, 2020 08:11:00');
-let dt2 = new Date('October 13, 2021 11:13:00');
-console.log(getDiff(dt1, dt2));
