caf5a57d415ec00b35a2c8f61b4aaa8d132902e2
Valentinka2222 <xamalyan89@gmail.com>
2022-04-01 13:31:13 +0300

    math.floor

--- lesson24/homeTask1/index.js
+++ lesson24/homeTask1/index.js
@@ -2,17 +2,17 @@ export const getDiff = (startDate, endDate) => {
   let diff = Math.abs(endDate - startDate);
 
   diff /= 1000;
-  const days = Math.ceil(diff / 60 / 60 / 24);
-  const hours = Math.ceil((diff / 60 / 60) % 24);
-  const minutes = Math.ceil((diff / 60) % 60);
-  const seconds = Math.ceil(diff % 60);
+  const days = Math.floor(diff / 60 / 60 / 24);
+  const hours = Math.floor((diff / 60 / 60) % 24);
+  const minutes = Math.floor((diff / 60) % 60);
+  const seconds = Math.floor(diff % 60);
 
   const dateStr = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   return dateStr;
 };
 
-const dt1 = new Date(Date.UTC(2022, 3, 20, 5, 25, 50));
-const dt2 = new Date(Date.UTC(2023, 7, 10, 5, 40, 45));
+const dt1 = new Date(Date.UTC(2022, 3, 20, 5, 3, 29));
+const dt2 = new Date(Date.UTC(2022, 7, 22, 5, 0, 45));
 console.log(getDiff(dt2, dt1));
 console.log(getDiff(dt1, dt2));
