4e844e2af47dcb506e2abadbeaf45573e0c8bfce
Valentinka2222 <xamalyan89@gmail.com>
2022-04-01 13:23:23 +0300

    math.ceil

--- lesson24/homeTask1/index.js
+++ lesson24/homeTask1/index.js
@@ -2,10 +2,10 @@ export const getDiff = (startDate, endDate) => {
   let diff = Math.abs(endDate - startDate);
 
   diff /= 1000;
-  const days = Math.round(diff / 60 / 60 / 24);
-  const hours = Math.round((diff / 60 / 60) % 24);
-  const minutes = Math.round((diff / 60) % 60);
-  const seconds = Math.round(diff % 60);
+  const days = Math.ceil(diff / 60 / 60 / 24);
+  const hours = Math.ceil((diff / 60 / 60) % 24);
+  const minutes = Math.ceil((diff / 60) % 60);
+  const seconds = Math.ceil(diff % 60);
 
   const dateStr = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
