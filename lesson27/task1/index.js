01d0564f0bb746a3944dadeef758fe645eab6b86
Valentinka2222 <xamalyan89@gmail.com>
2022-04-05 11:59:11 +0300

    L27t1

---
+++ lesson27/task1/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
