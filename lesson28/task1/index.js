2db73486e9248c951c8fc8822ddf2a370736860a
Valentinka2222 <xamalyan89@gmail.com>
2022-04-06 14:43:55 +0300

    L28t1

---
+++ lesson28/task1/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const calc = (initNumber) => {
+  let result = initNumber;
+  const calculator = {
+    add(number) {
+      result += number;
+      return calculator;
+    },
+    subtract(number) {
+      result -= number;
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+  return calculator;
+};
+const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
+console.log(result);
