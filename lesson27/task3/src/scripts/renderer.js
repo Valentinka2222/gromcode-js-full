09320b8bb88695735ad949ab36fe6318f930e6b1
Valentinka2222 <xamalyan89@gmail.com>
2022-04-06 12:55:12 +0300

    L27t3

---
+++ lesson27/task3/src/scripts/renderer.js
@@ -0,0 +1,42 @@
+import { getItem } from './storage.js';
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const listElem = document.querySelector('.list');
+const compareTasks = (a, b) => {
+  tasks.createDate = new Date().toISOString();
+  return new Date(b.createDate) - new Date(a.createDate);
+};
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.setAttribute('data-id', id);
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list__item-checkbox');
+  return checkboxElem;
+};
+
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement('li');
+  listItemElem.classList.add('list__item');
+  const checkboxElem = createCheckbox({ done, id });
+  if (done) {
+    listItemElem.classList.add('list__item_done');
+  }
+  listItemElem.append(checkboxElem, text);
+  return listItemElem;
+};
+
+export const renderTasks = () => {
+  const tasksList = getItem('taskList') || [];
+  listElem.innerHTML = '';
+  const tasksElems = tasksList.sort(compareTasks).map(createListItem);
+  listElem.append(...tasksElems);
+};
