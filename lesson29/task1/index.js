eaa61c38ed090a5bda2d41d8f48c79cbde1ac310
Valentinka2222 <xamalyan89@gmail.com>
2022-04-08 12:40:50 +0300

    3

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -16,13 +16,13 @@ const addImage = (imgSrc, callback) => {
 const imgSrc =
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
