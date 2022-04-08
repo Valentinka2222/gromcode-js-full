9c01eea8dcb91624bd8ba8721d567ddc8dafc1ba
Valentinka2222 <xamalyan89@gmail.com>
2022-04-08 12:30:24 +0300

    1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -15,13 +15,13 @@ export const addImage = (imgSrc, callback) => {
 const imgSrc =
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 // callack function
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
