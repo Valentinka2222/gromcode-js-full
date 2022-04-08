e34a537292616e20ae50fffff545c625d690be70
Valentinka2222 <xamalyan89@gmail.com>
2022-04-08 13:57:39 +0300

    3

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -4,11 +4,10 @@ export const pinger = (count, period) => {
   let i = count;
   console.log('Ping');
   const interval = setInterval(() => {
-    if (i-- > 0) {
+    if (--i > 0) {
       console.log('Ping');
     } else {
       clearInterval(interval);
     }
   }, period);
 };
-pinger(5, 1000);
