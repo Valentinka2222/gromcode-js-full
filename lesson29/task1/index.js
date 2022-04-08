a65d5b35cb88d53f702f4738ad558373ad6dd6ab
Valentinka2222 <xamalyan89@gmail.com>
2022-04-08 13:39:17 +0300

    1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -26,9 +26,5 @@ const onImageLoaded = (error, imgElem) => {
 
   sizeElem.textContent = `${width} x ${height}`;
 };
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
 
 export { addImage };
--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * @param {number} count
- * @param {number} period
- * @return {undefined}
- */
 export const pinger = (count, period) => {
   const interval = setInterval(() => {
     console.log('Ping');
@@ -14,6 +9,4 @@ export const pinger = (count, period) => {
   }, count * period);
 };
 
-// examples
-pinger(5, 100); // makes 5 writes with 100 ms interval
-pinger(7, 150); // makes 7 writes with 1500 ms interval
+pinger(5, 1000);
