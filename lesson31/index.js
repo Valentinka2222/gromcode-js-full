8aefdf770712336f5b54fcdd99c308828077437e
Valentinka2222 <xamalyan89@gmail.com>
2022-04-13 14:15:10 +0300

    L31t1

---
+++ lesson31/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+export const requestUserData = (userId) => {
+  const p = new Promise((resolve, rejected) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        rejected(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          email: `${userId}'@example.com'`,
+        });
+      }, 1000);
+    }
+  });
+  return p;
+};
+requestUserData(589484).then((data) => console.log(data));
