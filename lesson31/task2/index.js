f6f49c6f1a6a412a4c51ff4fb2f16269036238d1
Valentinka2222 <xamalyan89@gmail.com>
2022-04-13 18:47:24 +0300

    ''

--- lesson31/task2/index.js
+++ lesson31/task2/index.js
@@ -3,7 +3,7 @@
 export const asyncCalculator = (numbr) => {
   const p = new Promise((resolve) => {
     setTimeout(() => {
-      console.log(`Initial value:${numbr}`);
+      console.log(`'Initial value:${numbr}'`);
       resolve(numbr);
     }, 500);
   })
@@ -12,7 +12,7 @@ export const asyncCalculator = (numbr) => {
         new Promise((resolve) => {
           setTimeout(() => {
             const result = value * value;
-            console.log(`Squared value:${result}`);
+            console.log(`'Squared value:${result}'`);
             resolve(result);
           }, 500);
         }),
@@ -21,11 +21,11 @@ export const asyncCalculator = (numbr) => {
       (value) =>
         new Promise((resolve) => {
           const result = value * 2;
-          console.log(`Doubled value:${result}`);
+          console.log(`'Doubled value:${result}'`);
           resolve(result);
         }),
     );
 
   return p;
 };
-asyncCalculator(5).then((data) => console.log(data));
+asyncCalculator(5).then((value) => console.log(value));
