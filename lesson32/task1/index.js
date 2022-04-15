c62feb411dda461e1c424795796fa5391e797106
Valentinka2222 <xamalyan89@gmail.com>
2022-04-15 15:01:09 +0300

    L31t1

---
+++ lesson32/task1/index.js
@@ -0,0 +1,23 @@
+const getValue = (numb) => {
+  const number = new Promise((resolve) => {
+    resolve(numb);
+  });
+  return number;
+};
+
+const asyncNumb1 = getValue(2);
+const asyncNumb2 = getValue(20);
+const asyncNumb3 = getValue(20);
+const getSum = (numbers) =>
+  numbers
+    .filter((value) => !isNaN(value))
+    .reduce((acc, num) => acc + Number(num), 0);
+
+export const asyncSum = (...asyncNumbers) => {
+  return Promise.all(asyncNumbers)
+    .then((numbers) => getSum(numbers))
+    .catch(() => Promise.reject(new Error("Can't calculate")));
+};
+asyncSum(asyncNumb1, asyncNumb2, asyncNumb3).then((result) =>
+  console.log(result),
+);
