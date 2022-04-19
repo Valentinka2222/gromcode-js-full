4b9556fb9029c774071e5c7f2a4894c300ed7e1e
Valentinka2222 <xamalyan89@gmail.com>
2022-04-19 17:03:24 +0300

    L33t1

---
+++ lesson33/task1/index.js
@@ -0,0 +1,28 @@
+const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+
+const defaultAvatar =
+  'https://avatars.githubusercontent.com/u/1342004?s=200&v=4';
+userAvatarElem.src = defaultAvatar;
+
+const fetchUserData = (userName) => {
+  fetch(`http://api.github.com/users/${userName}`).then((response) => {
+    response.json().then((result) => console.log(result));
+  });
+};
+
+const renderUserData = (UserData) => {
+  const { avatar_url, name, location } = UserData;
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location ? `from ${location}` : '';
+};
+
+const showUserBtnElem = document.querySelector('.name-form__btn ');
+const userNameInputElem = document.querySelector('.name-form__input');
+const onSearchUser = () => {
+  const userName = userNameInputElem.ariaValueMax;
+  fetchUserData(userName).then((UserData) => renderUserData(UserData));
+};
+showUserBtnElem.addEventListener('click', onSearchUser);
