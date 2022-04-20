e30b1022edb1ccdeddfa9b34652b9e46a57189e5
Valentinka2222 <xamalyan89@gmail.com>
2022-04-20 18:23:41 +0300

    url

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,9 +1,7 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 export function getTasksList() {
-  return fetch(`https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks`).then(
-    (response) => response.json(),
-  );
+  return fetch(baseUrl).then((response) => response.json());
 }
 
 export function getTaskById(taskId) {
