bfe72f7f43846e7dc60ea57ebfdf9e5722e5817a
Valentinka2222 <xamalyan89@gmail.com>
2022-04-20 17:14:26 +0300

    1

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -13,9 +13,10 @@ const fetchUserData = (userName) => {
 };
 
 const renderUserData = (userData) => {
-  const { name } = userData;
-
+  const { avatar_url, name, location } = userData;
+  userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
+  userLocationElem.textContent = location ? `from ${location}` : '';
 };
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
