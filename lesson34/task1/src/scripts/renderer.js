b924412f54601ed0c95a3ce7811b62e126d344c9
Valentinka2222 <xamalyan89@gmail.com>
2022-04-24 15:05:50 +0300

    L34t1

--- lesson34/task1/src/scripts/renderer.js
+++ lesson34/task1/src/scripts/renderer.js
@@ -1,6 +1,15 @@
 import { getItem } from './storage.js';
 
 const listElem = document.querySelector('.list');
+
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.setAttribute('data-id', id);
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list__item-checkbox');
+  return checkboxElem;
+};
 const compareTasks = (a, b) => {
   if (a.done - b.done !== 0) {
     return a.done - b.done;
@@ -11,15 +20,6 @@ const compareTasks = (a, b) => {
   return new Date(b.createDate) - new Date(a.createDate);
 };
 
-const createCheckbox = ({ done, id }) => {
-  const checkboxElem = document.createElement('input');
-  checkboxElem.setAttribute('type', 'checkbox');
-  checkboxElem.setAttribute('data-id', id);
-  checkboxElem.checked = done;
-  checkboxElem.classList.add('list__item-checkbox');
-  return checkboxElem;
-};
-
 const createListItem = ({ text, done, id }) => {
   const listItemElem = document.createElement('li');
   listItemElem.classList.add('list__item');
@@ -27,12 +27,20 @@ const createListItem = ({ text, done, id }) => {
   if (done) {
     listItemElem.classList.add('list__item_done');
   }
-  listItemElem.append(checkboxElem, text);
+  const textElem = document.createElement('span');
+  textElem.classList.add('crossed-text');
+  textElem.textContent = text;
+
+  const deleteBtnElem = document.createElement('button');
+  deleteBtnElem.classList.add('list__item__delete-btn');
+
+  listItemElem.append(checkboxElem, text, deleteBtnElem);
   return listItemElem;
 };
 
 export const renderTasks = () => {
   const tasksList = getItem('tasksList') || [];
+  console.log(tasksList);
   listElem.innerHTML = '';
   const tasksElems = tasksList.sort(compareTasks).map(createListItem);
   listElem.append(...tasksElems);
--- lesson34/task1/src/scripts/updateTask.js
+++ lesson34/task1/src/scripts/updateTask.js
@@ -1,25 +1,31 @@
 import { renderTasks } from './renderer.js';
 
 import { getItem, setItem } from './storage.js';
+import { getTasksList, updateTask } from './tasksGateway.js';
 
 export const onToggleTask = (e) => {
   const isCheckbox = e.target.classList.contains('list__item-checkbox');
+
   if (!isCheckbox) {
     return;
   }
-  const tasksList = getItem('tasksList');
-  const newTasksList = tasksList.map((task) => {
-    if (task.id === e.target.dataset.id) {
-      const done = e.target.checked;
-      return {
-        ...task,
-        done,
-        finishDate: done ? new Date().toISOString() : null,
-      };
-    }
-    return task;
-  });
-  setItem('tasksList', newTasksList);
+  const taskId = e.target.dataset.id;
 
-  renderTasks();
+  const tasksList = getItem('tasksList');
+  const { text, createDate } = tasksList.find((task) => task.id === taskId);
+  const done = e.target.checked;
+
+  const updatedTask = {
+    text,
+    createDate,
+    done,
+    finishDate: done ? new Date().toISOString() : null,
+  };
+
+  updateTask(taskId, updatedTask)
+    .then(() => getTasksList())
+    .then((newTasksList) => {
+      setItem('tasksList', newTasksList);
+      renderTasks();
+    });
 };
