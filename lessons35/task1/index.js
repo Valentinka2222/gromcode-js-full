b450e65b29b3e2295a8a463a69b3015970f2a109
Valentinka2222 <xamalyan89@gmail.com>
2022-04-25 22:40:22 +0300

    L35t1

---
+++ lessons35/task1/index.js
@@ -0,0 +1,29 @@
+window.addEventListener('error', function onUnhandledError(err) {
+  console.error('error', err.message);
+});
+
+let userParsingResult;
+
+try {
+  const jsonString = '{ "singer": "Weekend", "albom": "After Hours" }';
+
+  const user = JSON.parse(jsonString);
+  console.log('User data: ', user);
+  userParsingResult = 'success';
+} catch (e) {
+  userParsingResult = 'error';
+} finally {
+  console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+
+try {
+  const product = JSON.parse('{ "singer": "Weekend", "albom": "After Hours" ');
+  console.log('Product data: ', product);
+  productParsingResult = 'success';
+} catch (e) {
+  productParsingResult = 'error';
+} finally {
+  console.log(`Product parsing finished with ${productParsingResult}`);
+}
