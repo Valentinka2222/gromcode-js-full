4460d94c8c677bb0a1993aa7d869b4b49f2730c6
Valentinka2222 <xamalyan89@gmail.com>
2022-04-26 21:02:01 +0300

    default img

--- lessons35/task3/renderer.js
+++ lessons35/task3/renderer.js
@@ -2,7 +2,7 @@ import { createReposElem } from './createRep.js';
 
 import { fetchUserData, fetchRep } from './requests.js';
 
-const defaultAvatar = 'https://avatars.githubusercontent.com/u10004';
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 
 const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
