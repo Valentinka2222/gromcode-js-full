265842fb5c4a77b876ca9a5f90c8baaca09c8306
Valentinka2222 <xamalyan89@gmail.com>
2022-04-27 10:12:19 +0300

    3

--- lessons35/task3/index.js
+++ lessons35/task3/index.js
@@ -3,6 +3,6 @@ import { onSearchUser } from './requests.js';
 const showUserBtnElem = document.querySelector('.name-form__btn');
 showUserBtnElem.addEventListener('click', onSearchUser);
 
-/*window.addEventListener('unhandledrejection', function (event) {
+window.addEventListener('unhandledrejection', function (event) {
   alert(event.promise);
-});*/
+});
