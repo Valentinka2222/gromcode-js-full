6243f95f5924b6a37816fbddb987dd05d89ec734
Valentinka2222 <xamalyan89@gmail.com>
2022-04-27 10:32:50 +0300

    spinner

--- lessons35/task3/index.js
+++ lessons35/task3/index.js
@@ -1,8 +1,23 @@
 import { onSearchUser } from './requests.js';
 
+import { fetchRep, fetchUserData } from './requests.js';
+
 const showUserBtnElem = document.querySelector('.name-form__btn');
 showUserBtnElem.addEventListener('click', onSearchUser);
 
+const spinner = () => {
+  const spinnerElem = document.querySelector('.spinner_hidden');
+  spinnerElem.classList.remove('spinner_hidden');
+  /*try {
+    fetchRep, fetchUserData;
+  } catch (e) {
+    alert('Failed to load data');
+  } finally {
+    spinnerElem.classList.add('spinner_hidden');
+  }*/
+};
+
+showUserBtnElem.addEventListener('click', spinner);
 window.addEventListener('unhandledrejection', function (event) {
   alert(event.promise);
 });
