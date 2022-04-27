1f64e3d3dc6139296a8e24009a570d9dff301fb6
Valentinka2222 <xamalyan89@gmail.com>
2022-04-27 11:27:20 +0300

    repos list

--- lessons35/task3/renderer.js
+++ lessons35/task3/renderer.js
@@ -3,16 +3,15 @@ import { createReposElem } from './createRep.js';
 import { fetchUserData, fetchRep } from './requests.js';
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
-
 const userAvatarElem = document.querySelector('.user__avatar');
-const userNameElem = document.querySelector('.user__name');
-const userLocationElem = document.querySelector('.user__location');
-userAvatarElem.src = defaultAvatar;
 
+userAvatarElem.src = defaultAvatar;
 export const renderUserData = (userData) => {
+  const userNameElem = document.querySelector('.user__name');
+  const userLocationElem = document.querySelector('.user__location');
   createReposElem();
   const userReposUrlElem = document.querySelector('.repos_url');
-  const { avatar_url, name, location, repos_url } = userData;
+  const { name, location, repos_url, avatar_url } = userData;
   userLocationElem.textContent = location ? `from ${location}` : '';
   userReposUrlElem.textContent = repos_url;
   userAvatarElem.src = avatar_url;
@@ -21,12 +20,7 @@ export const renderUserData = (userData) => {
   fetchUserData(name);
 
   fetchRep(name);
-  const successRequest = Promise.resolve({
-    avatar_url,
-    name,
-    location,
-    repos_url,
-  });
+  const successRequest = Promise.resolve(userData);
   successRequest.then((data) => {
     if (data) {
       const spinnerElem = document.querySelector('.spinner');
