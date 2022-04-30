268399bb63b52c11882b4965a66d0bdf76837044
Valentinka2222 <xamalyan89@gmail.com>
2022-04-30 14:55:36 +0300

    L36t1

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,9 +1,15 @@
-'use strict';
-
-
-
-export const fetchUser = userId => {
+export const fetchUser = async userId => {
   try {
-  const response =await fetch(``)
-}
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (!response.ok) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to get user data');
+  }
 };
+fetchUser('facebook')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err.message));
